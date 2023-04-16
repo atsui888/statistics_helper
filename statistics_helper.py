9d2e0d056a1ed0dddc0bc208c1e54d0e7d5dad1c
Richard Chai <atsuishisen@gmail.com>
2023-04-16 14:28:17 +0800

    add population_mean_confidence_interval

--- statistics_helper.py
+++ statistics_helper.py
@@ -43,11 +43,15 @@ class ECDFHelper:
     methods - public
     ----------------
     .fit()
+    .mean_confidence_interval(data, confidence=0.95)
+        .test_mean_confidence_interval()
+    .median_confidence_interval(data, confidence=.95)
+        .test_median_confidence_interval()
     .plot_cdf()
     .get_cumulative_probability(x)
         .test_get_cumulative_probability(x)
-    .with_ci_get_x(confidence_level: float = 0.95):
-        . test_with_ci_get_x(cl=0.98):
+    .with_p_value_get_x(p: float = 0.5):
+        . test_with_p_value_get_x()
 
     methods - internal
     ------------------
@@ -99,6 +103,72 @@ class ECDFHelper:
             final_p_value = 1.0
             return final_x, final_p_value
 
+    def with_interval_get_p_value(self, x1: float, x2: float):
+        if not self._fitted:
+            msg = "Please fit ECDF with data first, otherwise all values are None"
+            print(f"\n{msg}")
+            raise SystemError(msg)
+
+        try:
+            if x1 <= x2:
+                msg = "The second number passed in must be larger than the first."
+                print(msg)
+                raise SystemError(msg)
+        except Exception as e:
+            print(e)
+
+        p_x1 = self.get_cumulative_probability(x1)
+        p_x2 = self.get_cumulative_probability(x2)
+        return p_x1, p_x2, p_x2-p_x1
+
+    def population_mean_confidence_interval(self, confidence=0.95):
+        # https://pythonguides.com/scipy-confidence-interval/
+        n = self._data.shape[0]
+        m = np.mean(self._data)
+        se = scipy.stats.sem(self._data)
+        h = se * scipy.stats.t.ppf((1 + confidence) / 2., n - 1)
+        return m, m-h, m+h
+
+    def population_median_confidence_interval(self, confidence=0.95):
+        pass
+
+
+# Testing Functions - below
+
+
+def test_population_mean_confidence_interval(confidence=0.95):
+    """
+    Given a sample, estimate the population mean
+    :param confidence:
+    :return:
+    """
+    df = pd.read_excel('DA-LSS.xlsx', sheet_name='THT', skiprows=8)
+    cols = ['tht', 'training', 'unknown']
+    df.columns = cols
+    df.drop(columns='unknown', inplace=True)
+
+    print(f"Sample Mean = {int(df['tht'].mean())}")
+
+    zz = ECDFHelper(df['tht'])
+    m, m_l, m_h = zz.population_mean_confidence_interval(confidence=confidence)
+    print(f"{m_l:,.0f}, {m:,.0f}, {m_h:,.0f}")
+    print(f"{confidence * 100}% Confidence Interval that the Population Mean is between {m_l:,.0f} and {m_h:,.0f}")
+
+
+def test_with_interval_get_p_value(n1: float, n2: float, show_chart=False):
+    df = pd.read_excel('DA-LSS.xlsx', sheet_name='THT', skiprows=8)
+    cols = ['tht', 'training', 'unknown']
+    df.columns = cols
+    df.drop(columns='unknown', inplace=True)
+    zz = ECDFHelper(df['tht'])
+    zz.fit()
+    if show_chart:
+        zz.plot_cdf()
+
+    p_n1, p_n2, p_interval = zz.with_interval_get_p_value(x1=n1, x2=n2)
+    print(f"\np_interval = {p_n2:,.2f} - {p_n1:,.2f} = {p_interval:,.2f}")
+    print(f"P({n1} < X < {n2}) = {p_interval:,.2f}")
+
 
 def test_get_cumulative_probability(x, show_chart=False):
     df = pd.read_excel('DA-LSS.xlsx', sheet_name='THT', skiprows=8)
@@ -134,7 +204,7 @@ def test_with_p_value_get_x(p=0.5, show_chart=True):
     print(f"The probability of values being less than {x_val} is {p}.")
 
 
-def mean_confidence_interval(data, confidence=0.95):
+def z_mean_confidence_interval(data, confidence=0.95):
     # https://pythonguides.com/scipy-confidence-interval/
     a = 1.0 * np.array(data)
     n = len(a)
@@ -145,7 +215,7 @@ def mean_confidence_interval(data, confidence=0.95):
     return m, m-h, m+h
 
 
-def median_confidence_interval(data, confidence=.95):
+def z_median_confidence_interval(data, confidence=.95):
     dx = data.sort_values(ascending=True, ignore_index=True)
     factor = statistics.NormalDist().inv_cdf((1+confidence)/2)
     factor *= math.sqrt(len(dx))
@@ -156,7 +226,7 @@ def median_confidence_interval(data, confidence=.95):
     return dx[lower_interval], dx[upper_interval]
 
 
-def test_median_confidence_interval():
+def z_test_median_confidence_interval():
     df = pd.read_excel('DA-LSS.xlsx', sheet_name='Caffeine_batches', skiprows=6)
 
     cols = ['batch_num', 'caffeine_pct', 'unknown']
@@ -165,11 +235,11 @@ def test_median_confidence_interval():
 
     data = df['caffeine_pct']
     cl = 0.95
-    low, high = median_confidence_interval(data, cl)
+    low, high = z_median_confidence_interval(data, cl)
     print(f"{cl*100}% Confidence Interval for Median is between {low:,.4f} and {high:,.4f}")
 
 
-def test_mean_confidence_interval():
+def z_test_mean_confidence_interval():
     df = pd.read_excel('DA-LSS.xlsx', sheet_name='Caffeine_batches', skiprows=6)
 
     cols = ['batch_num', 'caffeine_pct', 'unknown']
@@ -178,14 +248,18 @@ def test_mean_confidence_interval():
 
     data = df['caffeine_pct']
     cl = 0.95
-    m, m_l, m_h = mean_confidence_interval(data, confidence=cl)
+    m, m_l, m_h = z_mean_confidence_interval(data, confidence=cl)
 
     print(f"{cl*100}% Confidence Interval for Mean is between {m_l:,.4f} and {m_h:,.4f}")
 
 
 if __name__ == "__main__":
-    # test_mean_confidence_interval()
+    test_population_mean_confidence_interval()
     # test_median_confidence_interval()
-    X = [500, 501, 699, 700]
-    test_get_cumulative_probability(X, show_chart=True)
-    test_with_p_value_get_x(p=0.982, show_chart=False)
+
+    # # X = [500, 501, 699, 700]
+    # # test_get_cumulative_probability(X, show_chart=True)
+    # # test_with_p_value_get_x(p=0.982, show_chart=False)
+    #
+    # test_with_interval_get_p_value(n1=300, n2=350, show_chart=True)
+    pass
